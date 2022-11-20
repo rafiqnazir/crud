ce138c7bc0bd32abea1a164c6d25d126c11e0b50
rafiqnazir <nademnazir79@gmail.com>
2022-11-20 12:26:17 +0530

    CRUD added
    ROR  RESTFUL Crud added

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :update, :destroy]
+
+    def show
+        render json: @article
+    end
+
+    def index
+        @articles = Article.all
+        render json: @articles
+    end
+
+    def create
+        article = Article.new(title: params[:title], description: params[:description])
+        if article.save
+            render json: {article: article}
+        else
+            render json: {error: article.errors.full_messages}
+        end
+    end
+
+    def update
+        @article.title = params[:title] if params[:title]
+        @article.description = params[:description] if params[:description]
+        if @article.save
+            render json: {article: @article}
+        else
+            render json: {error: @article.errors.full_messages}
+        end
+    end
+
+    def destroy
+        if @article.destroy
+            render json: {message: "deleted suceess"}
+        else
+            render json: {error: @article.errors.full_messages}
+        end
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])    
+    end
+
+end
